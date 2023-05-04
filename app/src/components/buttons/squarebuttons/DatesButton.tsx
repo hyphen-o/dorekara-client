237335abc69519cfdc60d4a9f43e19a355fd378f
keigo-0314 <okamoto.keigo@g.wakayama-u.jp>
2023-05-04 17:36:46 +0900

    履歴一覧・詳細ページの作成

--- app/src/components/buttons/squarebuttons/DatesButton.tsx
+++ app/src/components/buttons/squarebuttons/DatesButton.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
 import SquareButton from './SquareButton'
-import TrashIcon from '@/components/icons/TrashIcon'
 import { useRouter } from 'next/router'
 
 type Props = {
@@ -9,21 +8,13 @@ type Props = {
 
 const DatesButton: FC<Props> = ({ date }) => {
   const router = useRouter()
-
-  const handleTrashIconClick = () => {
-    ;(async () => {
-    })()
-  }
-
   const handleButtonClick = () => {
     router.push(`/histories/${date}`)
   }
 
   return (
     <>
-      <SquareButton text={date} onButtonClick={handleButtonClick}>
-        <TrashIcon onIconClick={handleTrashIconClick}></TrashIcon>
-      </SquareButton>
+      <SquareButton text={date} onButtonClick={handleButtonClick} />
     </>
   )
 }
