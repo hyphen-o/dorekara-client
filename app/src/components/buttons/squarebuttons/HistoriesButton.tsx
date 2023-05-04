f1ec7e8a769dafa501db6ff378c9eb228fc3b350
hyphen-o <otekapu@gmail.com>
2023-05-04 17:37:22 +0900

    prettierの適用

--- app/src/components/buttons/squarebuttons/HistoriesButton.tsx
+++ app/src/components/buttons/squarebuttons/HistoriesButton.tsx
@@ -2,13 +2,11 @@ import { FC } from 'react'
 import SquareButton from './SquareButton'
 import { Song } from '@/redux/types/songSlice.type'
 
-
 type Props = {
   song: Song
 }
 
 const HistoriesButton: FC<Props> = ({ song }) => {
-
   return (
     <>
       <SquareButton text={song.name} song_key={song.key} />
