4cd7bcb2c1bda08106f0be7d21ef451f7ee3c48f
hyphen-o <otekapu@gmail.com>
2023-05-08 15:55:25 +0900

    カラオケ機能の追加

--- app/src/components/forms/KaraokeForm.tsx
+++ app/src/components/forms/KaraokeForm.tsx
@@ -5,31 +5,81 @@ import Dorekana from '../images/Dorekana'
 import Selector from '../inputs/Selector'
 import RoundButton from '../buttons/roundbuttons/RoundButton'
 import { styles } from '@/styles/components/forms/KaraokeForm.style'
+import { Song } from '@/redux/types/songSlice.type'
+import { useRouter } from 'next/router'
 
 const KaraokeForm: FC = () => {
-  const [isOpen, SetIsOpen] = useState<boolean>(false)
+  const router = useRouter()
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [song, setSong] = useState<Song>({
+    id: null,
+    name: null,
+    artist_id: null,
+    key: null
+  })
 
   const { register, handleSubmit } = useForm<KaraokeFormValues>()
 
+  //ローカルストレージの曲を削除
+  const deleteLocalSongs = (): void => {
+    localStorage.removeItem('songs')
+  }
+
   const onSubmit: SubmitHandler<KaraokeFormValues> = async (data) => {
     try {
-      console.log(data)
+      //ローカルストレージに存在する曲を取得する
+      const songs = JSON.parse(localStorage.getItem('songs'))[0]
+      //アーティストに指定があれば曲をフィルタリングする
+      const filtered_songs = songs.filter(song => {
+        if(data.artist_id) return song.artist_id == data.artist_id
+        else return song
+      })
 
-      SetIsOpen(true)
+      //曲がこれ以上ない場合にアラートを出す
+      if(!songs.length) {
+        alert('曲がもうありません！カラオケを終了します.')
+        deleteLocalSongs()
+        router.push('home')
+        return
+      }
+      
+      //条件に合う曲がこれ以上ない場合にアラートを出す
+      if(!filtered_songs.length) {
+        alert('条件に合う曲がもうありません！')
+        return
+      }
+
+      //ランダムなインデックスを生成し，曲を選ぶ
+      const random_index = Math.floor(Math.random() * filtered_songs.length)
+      const random_song = filtered_songs[random_index]
+      setSong(random_song)
+
+      //ローカルの曲からランダムに選んだ曲を取り除く
+      const new_songs = songs.filter((song) => song.id != random_song.id)
+      localStorage.setItem('songs', JSON.stringify([new_songs]))
+
+      setIsOpen(true)
     } catch (error) {
       return error
     }
   }
 
-  const handleRoundButtonClick = () => {}
+  const handleRoundButtonClick = (): void => {
+    deleteLocalSongs()
+    router.push('home')
+  }
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} css={styles.form}>
         <div css={styles.text}>
           {
-            //hard
-            !isOpen ? 'TAP!' : '曲名'
+            isOpen ? song.name : 'TAP!'
+          }
+        </div>
+        <div>
+          {
+            isOpen && song.artist_id
           }
         </div>
         <Dorekana isOpen={isOpen}></Dorekana>
@@ -51,7 +101,7 @@ const KaraokeForm: FC = () => {
         <RoundButton
           theme='red'
           text='次の曲！'
-          handleButtonClick={() => SetIsOpen(false)}
+          handleButtonClick={() => setIsOpen(false)}
         ></RoundButton>
       )}
     </>
