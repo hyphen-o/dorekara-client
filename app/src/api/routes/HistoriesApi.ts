9b6959585508fa67604762f78eebfccfe0d4c258
keigo-0314 <okamoto.keigo@g.wakayama-u.jp>
2023-05-08 16:02:52 +0900

    カラオケ履歴作成機能の追加

--- app/src/api/routes/HistoriesApi.ts
+++ app/src/api/routes/HistoriesApi.ts
@@ -4,5 +4,6 @@ export const historyApi = {
   getDates: (user_id) => axiosClient(null).get(`history/${user_id}/all`),
   getSongs: (user_id, params) =>
     axiosClient(null).get(`history/${user_id}/songs`, { params }),
+  create: (user_id, params) => axiosClient(null).post(`history/${user_id}/create`, params),
   destroy: (params) => axiosClient(null).delete('history/destroy', { params }),
 }
