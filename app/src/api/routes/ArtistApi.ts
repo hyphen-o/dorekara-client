d753e14ea68b8e52508ee68ad6d19b5b80d92708
hyphen-o <otekapu@gmail.com>
2023-05-08 16:29:40 +0900

    アーティスト名，キーを表示するように変更

--- app/src/api/routes/ArtistApi.ts
+++ app/src/api/routes/ArtistApi.ts
@@ -2,6 +2,7 @@ import { axiosClient } from '@/api/axiosClient'
 
 export const artistApi = {
   getAll: (user_id) => axiosClient(null).get(`artist/${user_id}/all`),
+  getOne: (artist_id) => axiosClient(null).get(`artist/${artist_id}`),
   create: (user_id, params) =>
     axiosClient(null).post(`artist/${user_id}/create`, params),
   destroy: (params) => axiosClient(null).delete(`artist/destroy`, { params }),
