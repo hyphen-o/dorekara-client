a5e02e3c1fc9aaed9e531ee3a5972833b79453ef
keigo-0314 <okamoto.keigo@g.wakayama-u.jp>
2023-05-09 16:10:41 +0900

    画像upload

--- app/src/api/routes/UserApi.ts
+++ app/src/api/routes/UserApi.ts
@@ -2,4 +2,5 @@ import { axiosClient } from '@/api/axiosClient'
 
 export const userApi = {
   getImage: (id) => axiosClient(null).get(`image/${id}`),
+  uploadImage: (id, params) => axiosClient(null, 'multipart/form-data; boundary=<calculated when request is sent>').post(`image/${id}`, params),
 }
