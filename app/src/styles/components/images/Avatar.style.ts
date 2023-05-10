89920e6854ced1fe57dab34b3581cc19738650da
hyphen-o <otekapu@gmail.com>
2023-05-10 12:40:21 +0900

    ユーザ設定画面の作成

---
+++ app/src/styles/components/images/Avatar.style.ts
@@ -0,0 +1,20 @@
+import { color } from '@/styles/theme'
+import { css } from '@emotion/react'
+
+export const styles = {
+  wrapper: css`
+    margin: 0 auto;
+  `,
+  image: css`
+    border-radius: 50%;
+    margin: 0 auto;
+    width: 60px;
+    height: 60px;
+    border: solid ${color.darkgray};
+  `,
+  name: css`
+    color: ${color.white};
+    font-size: 10px;
+    text-align: center;
+  `
+}
