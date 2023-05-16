730371fb7de4abd23d2cbd6944abb658ac629000
hyphen-o <otekapu@gmail.com>
2023-05-16 16:50:20 +0900

    prettierの適用

--- app/src/components/seos/DefaultSeo.tsx
+++ app/src/components/seos/DefaultSeo.tsx
@@ -1,25 +1,24 @@
-import { DefaultSeo } from "next-seo"
-import Head from "next/head"
-
+import { DefaultSeo } from 'next-seo'
+import Head from 'next/head'
 
 const DefaultSEO = () => {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" href="/images/dorekana/shortcut.png" />
+        <link rel='shortcut icon' href='/images/dorekana/shortcut.png' />
       </Head>
-      <DefaultSeo 
+      <DefaultSeo
         defaultTitle='どれカラ -カラオケ選曲アプリ-'
         description='どれカラはカラオケで歌う曲を自分でまとめて，ランダムに選曲してくれるアプリです．'
         openGraph={{
           type: 'website',
           title: 'どれカラ',
-          description: 'どれカラはカラオケで歌う曲を自分でまとめて，ランダムに選曲してくれるアプリです．',
+          description:
+            'どれカラはカラオケで歌う曲を自分でまとめて，ランダムに選曲してくれるアプリです．',
           site_name: 'どれカラ',
           url: 'http://ec2-54-250-152-70.ap-northeast-1.compute.amazonaws.com/login',
         }}
       />
-
     </>
   )
 }
