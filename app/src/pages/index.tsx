24efbb1bb13fa7c8a8b2af64df7b843562247af3
hyphen-o <otekapu@gmail.com>
2023-07-29 01:22:24 +0900

    format

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -6,13 +6,13 @@ import Image from 'next/image'
 
 const Index: NextPage = () => {
   return (
-      <main css={styles.wrapper}>
-        <div css={styles.wrapper}>
-          <div css={styles.logoWrapper}>
-            <AppLogo css={styles.image}/>
-            <p css={styles.title}>カラオケ選曲アプリ</p>
-          </div>
-          <div css={styles.textWrapper}>
+    <main css={styles.wrapper}>
+      <div css={styles.wrapper}>
+        <div css={styles.logoWrapper}>
+          <AppLogo css={styles.image} />
+          <p css={styles.title}>カラオケ選曲アプリ</p>
+        </div>
+        <div css={styles.textWrapper}>
           <Image
             src='/images/icons/Karaoke-pana.svg'
             alt='dorekara-logo'
@@ -20,15 +20,15 @@ const Index: NextPage = () => {
             width={300}
             height={300}
           ></Image>
-            <p css={styles.text}>「一曲目何歌おうかな...」</p>
-            <p css={styles.text}>「次の曲を決めるのに時間がかかる」</p>
-            <br />
-            <p css={styles.text}>カラオケでこんな経験ありませんか？</p>
-            <p css={styles.text}>どれカラは，そんな人たちに向けたアプリです</p>
-          </div>
-          <TransButton text='早速試してみる！' pageLink='/register' />
+          <p css={styles.text}>「一曲目何歌おうかな...」</p>
+          <p css={styles.text}>「次の曲を決めるのに時間がかかる」</p>
+          <br />
+          <p css={styles.text}>カラオケでこんな経験ありませんか？</p>
+          <p css={styles.text}>どれカラは，そんな人たちに向けたアプリです</p>
         </div>
-      </main>
+        <TransButton text='早速試してみる！' pageLink='/register' />
+      </div>
+    </main>
   )
 }
 
